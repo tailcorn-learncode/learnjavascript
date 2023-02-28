9d0250bb4be1c0812e077767d1ee4282de772786
info <118862628+tailcornhub@users.noreply.github.com>
2023-02-27 21:29:17 -0500

    Create index.js

---
+++ js2/express/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+    apiKey: "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",
+});
+
+ const openai = new OpenAIApi(configuration);
+
+app.get('/',async (req, res) => {
+  console.log("hjjj");
+  let completion = await openai.createCompletion({
+      model: "davinci:ft-personal-2023-02-28-00-54-13",
+      prompt: "office of Retirement Services address",
+      temperature: 0,
+      max_tokens: 50
+  });
+  
+  console.log(completion.data.choices[0].text);
+  res.send(completion.data.choices[0].text)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
